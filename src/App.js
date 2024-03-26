ce7bab670a57122dba0e401c6e6ae431b41a60b9
iBobbyTS <ibobbyts@gmail.com>
2024-03-25 20:33:48 -0600

    Add Productpage to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Homepage from './component/Homepage';
+import Productpage from "./component/Productpage";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Router>
                 <Routes>
                     <Route path="/" element={<Homepage />} />
+                    <Route path="/products" element={<Productpage />} />
                 </Routes>
         </Router>
     );
